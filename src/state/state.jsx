fc502a304aa4e7d4cbba1aa4e73dfb1b865ea35b
litu3000 <acersytec@i.ua>
2024-05-30 23:22:05 +0300

    hw_97

---
+++ src/state/state.jsx
@@ -0,0 +1,19 @@
+import {Component} from 'react';
+
+
+
+class State extends Component {
+    constructor(props) {
+        super(props);
+        console.log(this)
+    }
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default State
