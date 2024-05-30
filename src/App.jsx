e81a13802f17a3628429644974a53d27a2b11525
litu3000 <acersytec@i.ua>
2024-05-30 23:35:15 +0300

    hw_97

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ function App() {
   return (
     <>
         <State/>
+
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
