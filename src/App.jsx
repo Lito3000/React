928bca09e66056098b2a06ef32c1eab93bd5cc72
litu3000 <acersytec@i.ua>
2024-06-07 23:11:00 +0300

    hw_95

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import './App.css'
 import State from "./state/state.jsx";
 import ListAdd from "./components/ListAdd.jsx";
 import TodoBox from "./components/TodoBox.jsx";
+import {Card} from "react-bootstrap";
 // import OtherFiles from "./Otherfiles/OtherFiles.jsx";
 // import OtherFiles2 from "./Otherfiles/OtherFiles2.jsx";
 // import AutoselectingInput from "./components/TodoBox.jsx";
@@ -13,29 +14,40 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-        {/*<AutoselectingInput/>*/}
-        {/*<OtherFiles2/>*/}
-        {/*<OtherFiles/>*/}
-        <TodoBox/>
-        {/*<ListAdd/>*/}
-        {/*<State/>*/}
-
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-    </>
+      <>
+          {/*<AutoselectingInput/>*/}
+          {/*<OtherFiles2/>*/}
+          {/*<OtherFiles/>*/}
+          <TodoBox/>
+          {/*<ListAdd/>*/}
+          {/*<State/>*/}
+          <Card>
+              <Card.Body>
+                  <Card.Title>Title</Card.Title>
+                  <Card.Text>Text</Card.Text>
+              </Card.Body>
+          </Card>
+          <div className="card">
+              <div className="card-body">
+                  <h4 className="card-title">Title</h4>
+                  <p className="card-text">Text</p>
+              </div>
+          </div>
+          <div>
+              <a href="https://vitejs.dev" target="_blank">
+                  <img src={viteLogo} className="logo" alt="Vite logo"/>
+              </a>
+          </div>
+          <h1>Vite + React</h1>
+          <div className="card">
+              <button onClick={() => setCount((count) => count + 1)}>
+                  count is {count}
+              </button>
+              <p>
+                  Edit <code>src/App.jsx</code> and save to test HMR
+              </p>
+          </div>
+      </>
   )
 }
 
