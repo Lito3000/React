e17bd43ff5a38fc6bc7563b9332dfd17f1e7b82d
litu3000 <acersytec@i.ua>
2024-06-27 23:49:18 +0300

    hw_103

--- src/components/ToDoBox.jsx
+++ src/components/ToDoBox.jsx
@@ -1,77 +1,44 @@
-// useState – встроенный в React хук
-import React, { useState } from 'react';
-
+import {useState} from 'react';
+import {cloneDeep, uniqueId} from 'lodash';
 
 const HookComponent = (props) => {
-    // Пример хука для работы с состоянием
-    const [count, setCount] = useState(0);
-    const [userName, setUserName] = useState('');
 
-    const handleClick = () => {
-        setCount(count + 1)
+    const [TodoBox, setTodoBox] = useState('');
+    const [SaveData, setSaveData] = useState([]);
+
+    // const handleClick = () => {
+    //     setCount(count + 1)
+    // }
+
+    const handleChange = (event) => {
+        const previousState = cloneDeep(TodoBox)
+        setTodoBox(previousState)
+        setTodoBox(event.target.value)
     }
 
-    const handleChange = (e) => {
-        setUserName(e.target.value)
-        console.log(userName)
-    }
-
-    
 
     return (
         <div>
             <div className="mb-3">
                 <form className="d-flex">
                     <div className="me-3">
-                        <input type="text"
-                               // value={userName}
-                               onChange={handleChange}
-                               required=""
-                               className="form-control"
-                               placeholder="I am going..."/>
+                        <input
+                            type="text"
+                            name='TodoBox'
+                            value={TodoBox}
+                            required=""
+                            className="form-control"
+                            placeholder="I am going..."
+                            onChange={handleChange}
+                        />
                     </div>
-                    <button type="submit" className="btn btn-primary" onClick={} >add</button>
+                    <button type="submit"
+                            className="btn btn-primary">add
+                    </button>
                 </form>
             </div>
-            <div>
-                <div className="row">
-                    <div className="col-auto">
-                        <button type="button" className="btn btn-primary btn-sm">-</button>
-                    </div>
-                    <div className="col">{userName}</div>
-                </div>
-                <hr/>
-            </div>
-            <div>
-                <div className="row">
-                    <div className="col-auto">
-                        <button type="button" className="btn btn-primary btn-sm">-</button>
-                    </div>
-                    <div className="col">first</div>
-                </div>
-                <hr/>
-            </div>
+            {/*{this.state.formData.saveData.map(item => this.renderFunction(item))}*/}
         </div>
-        // <div>
-        //     <div className="mb-3">
-        //         <form className="d-flex">
-        //             <div className="me-3">
-        //                 <input type="text" value="" required="" className="form-control" placeholder="I am going..."/>
-        //             </div>
-        //             <button type="submit" className="btn btn-primary">add</button>
-        //         </form>
-        //     </div>
-        // </div>
-
-        // <div>
-        //     <p>Вы нажали {count} раз(а)</p>
-        //     <button onClick={handleClick}>
-        //         Нажми меня
-        //     </button>
-        //     <hr/>
-        //     <div>User Name: {userName}</div>
-        //     <input type="text" value={userName} onChange={handleChange}/>
-        // </div>
     );
 };
 
