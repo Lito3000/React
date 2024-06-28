55344b17faebab9ac35ec57010cf4554b19c5681
litu3000 <acersytec@i.ua>
2024-06-28 19:23:53 +0300

    hw_104

--- src/components/MarkdownEditor.jsx
+++ src/components/MarkdownEditor.jsx
@@ -1,14 +1,15 @@
 import {useEffect, useRef} from "react";
 import Editor from '@toast-ui/editor';
-import '@toast-ui/editor/dist/toastui-editor.css'; // Editor's Style
+import '@toast-ui/editor/dist/toastui-editor.css';
+import PropTypes from "prop-types";
 
 const MarkdownEditor = (item) => {
 
 
-    const textRef = useRef(null)
+    const textEditor = useRef(null)
     useEffect(() => {
         const editor = new Editor({
-            el: textRef.current,
+            el: textEditor.current,
             hideModeSwitch: true,
             height: '500px',
             initialEditType: 'markdown',
@@ -24,10 +25,12 @@ const MarkdownEditor = (item) => {
 
     return (
         <>
-            <div ref={textRef}></div>
+            <div ref={textEditor}></div>
 
         </>
     );
 }
-
+MarkdownEditor.propTypes = {
+   textView: PropTypes.func.isRequired,
+};
 export default MarkdownEditor;
