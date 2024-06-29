a81491787fda004330382751066923603dfcf702
litu3000 <acersytec@i.ua>
2024-06-29 10:52:48 +0300

    hw_105

---
+++ src/components/MarkdownEditor.jsx
@@ -0,0 +1,35 @@
+import {useEffect, useRef} from "react";
+import Editor from '@toast-ui/editor';
+import '@toast-ui/editor/dist/toastui-editor.css';
+import PropTypes from "prop-types";
+
+const MarkdownEditor = ({textView}) => {
+
+
+    const textEditor = useRef(null)
+    useEffect(() => {
+        const editor = new Editor({
+            el: textEditor.current,
+            hideModeSwitch: true,
+            height: '500px',
+            initialEditType: 'markdown',
+            previewStyle: 'vertical'
+        });
+        console.log(editor)
+
+        editor.addHook('change', () => {
+            const content = editor.getMarkdown();
+            textView(content)
+        });
+    },[textView]);
+
+    return (
+        <>
+            <div ref={textEditor}></div>
+        </>
+    );
+};
+MarkdownEditor.propTypes = {
+    textView: PropTypes.func.isRequired,
+};
+export default MarkdownEditor;
