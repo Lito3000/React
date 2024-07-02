7595b267e2a1a3e8231dbc2f5ff1b31004665b23
litu3000 <acersytec@i.ua>
2024-07-02 23:05:06 +0300

    hw_106

---
+++ src/components/ToDoBox.jsx
@@ -0,0 +1,64 @@
+import {useState} from 'react';
+import _, {uniqueId} from 'lodash';
+import InputTodoBox from "./InputTodoBox.jsx";
+import PropTypes from "prop-types";
+
+const HookComponent = () => {
+
+    const [TodoBox, setTodoBox] = useState('');
+    const [SaveData, setSaveData] = useState([]);
+    const handleChange = (event) => {
+        setTodoBox(event.target.value)
+    }
+
+    const handleSabmit  = (e) => {
+        e.preventDefault();
+        if (TodoBox.trim() !== '') {
+            const newData = {
+                idData: uniqueId(),
+                saveData: TodoBox
+            };
+            setSaveData([newData, ...SaveData]);
+            setTodoBox('');
+        }
+    };
+    const removeItem = (id) => (e) => {
+        e.preventDefault();
+        const todoData = _.cloneDeep(SaveData)
+        const newItems = todoData.filter(function (item) {
+            if (item.idData !== id) {
+                return item
+            }
+        });
+        setSaveData(newItems)
+    };
+    const renderFunction = (item) => {
+        return <div key={item.saveData + uniqueId('iddDiv')} className="row">
+            <div className="col-auto">
+                <button type="button"
+                        onClick={removeItem(item.idData)}
+                        className="btn btn-primary btn-sm">-
+                </button>
+            </div>
+            <div className="col">{item.saveData}</div>
+            <hr/>
+        </div>
+    }
+
+    return (
+        <div>
+            <InputTodoBox Change={handleChange} Click={handleSabmit} value={TodoBox}/>
+            {SaveData.map(item => renderFunction(item))}
+        </div>
+    );
+};
+HookComponent.propTypes = {
+    saveData: PropTypes.arrayOf(
+        PropTypes.shape({
+            idData: PropTypes.string.isRequired,
+            saveDate: PropTypes.string.isRequired
+        })
+    ),
+    TodoBox: PropTypes.string
+};
+export default HookComponent;
